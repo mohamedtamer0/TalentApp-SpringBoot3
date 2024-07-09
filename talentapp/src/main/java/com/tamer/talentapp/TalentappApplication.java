5d9d018cf7e5648efe9071fef62254e175806dd4
mohamedtamer0 <mt4452242@gmail.com>
2024-07-09 20:22:41 +0300

    Create Spring Project

---
+++ talentapp/src/main/java/com/tamer/talentapp/TalentappApplication.java
@@ -0,0 +1,13 @@
+package com.tamer.talentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentappApplication.class, args);
+	}
+
+}
---
+++ talentapp/src/test/java/com/tamer/talentapp/TalentappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamer.talentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
