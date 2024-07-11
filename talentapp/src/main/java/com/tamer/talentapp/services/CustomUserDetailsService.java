acc610ecbc7603b30f1c954a9510b29914db0366
mohamedtamer0 <mt4452242@gmail.com>
2024-07-11 20:42:49 +0300

    update

---
+++ talentapp/src/main/java/com/tamer/talentapp/services/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.tamer.talentapp.services;
+
+
+import com.tamer.talentapp.entity.Users;
+import com.tamer.talentapp.repository.UsersRepository;
+import com.tamer.talentapp.util.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found user"));
+        return new CustomUserDetails(user);
+    }
+}
