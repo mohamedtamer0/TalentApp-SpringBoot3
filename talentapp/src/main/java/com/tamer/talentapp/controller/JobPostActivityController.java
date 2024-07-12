5124d78158c369cd5bb784549cef14bb17013cca
mohamedtamer0 <mt4452242@gmail.com>
2024-07-12 21:13:07 +0300

    update

--- talentapp/src/main/java/com/tamer/talentapp/controller/JobPostActivityController.java
+++ talentapp/src/main/java/com/tamer/talentapp/controller/JobPostActivityController.java
@@ -2,12 +2,15 @@ package com.tamer.talentapp.controller;
 
 
 import com.tamer.talentapp.entity.JobPostActivity;
+import com.tamer.talentapp.entity.RecruiterJobsDto;
+import com.tamer.talentapp.entity.RecruiterProfile;
 import com.tamer.talentapp.entity.Users;
 import com.tamer.talentapp.services.JobPostActivityService;
 import com.tamer.talentapp.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -37,6 +41,10 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
--- talentapp/src/main/java/com/tamer/talentapp/services/JobPostActivityService.java
+++ talentapp/src/main/java/com/tamer/talentapp/services/JobPostActivityService.java
@@ -1,9 +1,13 @@
 package com.tamer.talentapp.services;
 
 
-import com.tamer.talentapp.entity.JobPostActivity;
+import com.tamer.talentapp.entity.*;
 import com.tamer.talentapp.repository.JobPostActivityRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class JobPostActivityService {
 
@@ -15,5 +19,26 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity) {
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter) {
+
+        List<IRecruiterJobs> recruiterJobsDtos = jobPostActivityRepository.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for (IRecruiterJobs rec : recruiterJobsDtos) {
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(),
+                    rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
+
+    }
+
+    public JobPostActivity getOne(int id) {
+
+        return jobPostActivityRepository.findById(id).orElseThrow(()->new RuntimeException("Job not found"));
     }
 }
