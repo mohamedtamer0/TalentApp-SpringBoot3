69443557fda3ee0037139680dfd6d9dea1e2cdb8
mohamedtamer0 <mt4452242@gmail.com>
2024-07-13 12:59:44 +0300

    update

--- talentapp/src/main/java/com/tamer/talentapp/entity/JobSeekerProfile.java
+++ talentapp/src/main/java/com/tamer/talentapp/entity/JobSeekerProfile.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class JobSeekerProfile {
 
     @Id
-    private int userAccountId;
+    private Integer userAccountId;
 
     @OneToOne
     @JoinColumn(name = "user_account_id")
@@ -38,7 +38,7 @@ public class JobSeekerProfile {
         this.userId = userId;
     }
 
-    public JobSeekerProfile(int userAccountId, Users userId, String firstName, String lastName, String city, String state, String country, String workAuthorization, String employmentType, String resume, String profilePhoto, List<Skills> skills) {
+    public JobSeekerProfile(Integer userAccountId, Users userId, String firstName, String lastName, String city, String state, String country, String workAuthorization, String employmentType, String resume, String profilePhoto, List<Skills> skills) {
         this.userAccountId = userAccountId;
         this.userId = userId;
         this.firstName = firstName;
@@ -53,11 +53,11 @@ public class JobSeekerProfile {
         this.skills = skills;
     }
 
-    public int getUserAccountId() {
+    public Integer getUserAccountId() {
         return userAccountId;
     }
 
-    public void setUserAccountId(int userAccountId) {
+    public void setUserAccountId(Integer userAccountId) {
         this.userAccountId = userAccountId;
     }
 
@@ -149,6 +149,12 @@ public class JobSeekerProfile {
         this.skills = skills;
     }
 
+    @Transient
+    public String getPhotosImagePath() {
+        if (profilePhoto == null || userAccountId == null) return null;
+        return "/photos/candidate/" + userAccountId + "/" + profilePhoto;
+    }
+
     @Override
     public String toString() {
         return "JobSeekerProfile{" +
@@ -163,7 +169,6 @@ public class JobSeekerProfile {
                 ", employmentType='" + employmentType + '\'' +
                 ", resume='" + resume + '\'' +
                 ", profilePhoto='" + profilePhoto + '\'' +
-                ", skills=" + skills +
                 '}';
     }
 }
---
+++ talentapp/src/main/java/com/tamer/talentapp/services/JobSeekerProfileService.java
@@ -0,0 +1,26 @@
+package com.tamer.talentapp.services;
+
+
+import com.tamer.talentapp.entity.JobSeekerProfile;
+import com.tamer.talentapp.repository.JobSeekerProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+
+    public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
+        return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+}
